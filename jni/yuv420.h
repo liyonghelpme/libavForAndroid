1be5e9150e7e8258b71fc3671daa773341eb07d7
liyong <liyonghelpme@gmail.com>
2013-03-19 15:41:56 +0800

    yuv420

---
+++ jni/yuv420.h
@@ -0,0 +1,29 @@
+#ifndef __YUV420_H__
+#define __YUV420_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "libavutil/pixfmt.h"
+#include "libswscale/swscale.h"
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include "libavutil/colorspace.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/mathematics.h"
+AVCodecContext *video_init(int width, int height, int frameRate);
+AVFrame *video_initFrame(AVCodecContext *c, uint8_t **pb);
+void video_initBuf(AVCodecContext *c, int *outbuf_size, uint8_t **outbuf);
+void compressPicture(AVCodecContext *c, AVFrame *picture); 
+void video_clean(FILE *f, uint8_t *picture_buf, uint8_t *outbuf, AVCodecContext *c, AVFrame *picture);
+void open_video(AVFormatContext *oc, AVStream *st, uint8_t **outbuf, int *outSize, AVPicture **pic); 
+AVStream *add_video_stream(AVFormatContext *oc, enum CodecID codec_id, int width, int height); 
+#ifdef __cplusplus
+}
+#endif
+
+#include <android/log.h>
+#define  LOG_TAG    "libgl2jni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#endif
